de047672e5581359792c10a6824666f52aac3af0
Omer Ozarslan <omerfaruko@gmail.com>
2018-06-01 06:23:05 -0500

    Implement import/export from file

---
+++ src/gzidx.c
@@ -0,0 +1,27 @@
+#include "gzidx.h"
+
+int gzidx_import(gzidx_index *index, FILE* input_index_file)
+{
+    const gzidx_gzip_index_stream input_stream = {
+        gzidx_raw_file_read,
+        gzidx_raw_file_write,
+        gzidx_raw_file_seek,
+        gzidx_raw_file_eof,
+        gzidx_raw_file_error,
+        (void*) input_index_file
+    };
+    return gzidx_import_advanced(index, &input_stream, NULL);
+}
+
+int gzidx_export(gzidx_index *index, FILE* output_index_file)
+{
+    const gzidx_gzip_index_stream output_stream = {
+        gzidx_raw_file_read,
+        gzidx_raw_file_write,
+        gzidx_raw_file_seek,
+        gzidx_raw_file_eof,
+        gzidx_raw_file_error,
+        (void*) output_index_file
+    };
+    return gzidx_export_advanced(index, &output_stream, NULL);
+}
