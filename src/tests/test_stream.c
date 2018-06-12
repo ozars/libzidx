839d265fdec13ea497a22cf901086efd9a090b09
Omer Ozarslan <omerfaruko@gmail.com>
2018-06-12 14:38:26 -0500

    Modify test to read multiple times

--- src/tests/test_stream.c
+++ src/tests/test_stream.c
@@ -2,29 +2,38 @@
 #include<string.h>
 #include<assert.h>
 
-#include<gzidx.h>
+#include<zidx.h>
 
 int main()
 {
-    FILE *f = tmpfile();
-    const char test_str[] = "It works!";
-    char read_str[sizeof(test_str)] = "000000000";
-    int str_len = sizeof(test_str);
+    FILE *f = fopen("testfile", "w");
+    const char test_str[] = "It works!12567890";
+    char buf[5] = "00000";
+    int buf_len = sizeof(buf);
+    int read_len;
 
-    assert(gzidx_raw_file_write(f, test_str, str_len) == str_len);
-    assert(gzidx_raw_file_eof(f) == 0);
-    assert(gzidx_raw_file_error(f) == 0);
-    assert(gzidx_raw_file_tell(f) == str_len);
-    assert(gzidx_raw_file_seek(f, 0, GZIDX_SEEK_SET) == 0);
-    assert(gzidx_raw_file_tell(f) == 0);
-
-    assert(gzidx_raw_file_read(f, read_str, str_len) == str_len);
-    assert(gzidx_raw_file_eof(f) == 0);
-    assert(gzidx_raw_file_error(f) == 0);
-    assert(gzidx_raw_file_read(f, read_str, 1) == 0);
-    assert(gzidx_raw_file_error(f) == 0);
-    assert(gzidx_raw_file_eof(f) > 0);
+    assert(f);
+    assert(zidx_raw_file_write(f, test_str, sizeof(test_str)) == sizeof(test_str));
+    assert(zidx_raw_file_eof(f) == 0);
+    assert(zidx_raw_file_error(f) == 0);
+    assert(zidx_raw_file_tell(f) == sizeof(test_str));
+    assert(zidx_raw_file_seek(f, 0, ZIDX_SEEK_SET) == 0);
+    assert(zidx_raw_file_tell(f) == 0);
 
     fclose(f);
+    f = fopen("testfile", "r");
+    assert(f);
+
+    while(1) {
+        read_len = zidx_raw_file_read(f, buf, buf_len);
+        for(int i = 0; i < read_len; i++)
+        {
+            printf("%02x ", buf[i]);
+        }
+        printf("\n");
+        assert(!zidx_raw_file_error(f));
+        if(zidx_raw_file_eof(f)) break;
+    }
+
     return 0;
 }
