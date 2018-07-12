6f783637cc0cd467e32cc4a843d760d8c029abaa
Omer Ozarslan <omerfaruko@gmail.com>
2018-07-12 17:03:24 -0500

    Change test_ext_dir flag from cached to created

--- travis-install.sh
+++ travis-install.sh
@@ -21,8 +21,8 @@ sudo apt-get update
 sudo apt-get install -y autoconf texinfo
 
 # Clone libraries needed for test if they are not cached
-mkdir "$TEST_EXT_DIR" || TEST_EXT_DIR_CACHED=1
-if [ $TEST_EXT_DIR_CACHED -ne 1 ]; then
+mkdir "$TEST_EXT_DIR" || TEST_EXT_DIR_CREATED=1
+if [ $TEST_EXT_DIR_CREATED -eq 1 ]; then
     cd "$TEST_EXT_DIR"
     git clone --branch "$LIBCHECK_TAG" https://github.com/libcheck/check
     git clone https://github.com/imneme/pcg-c
