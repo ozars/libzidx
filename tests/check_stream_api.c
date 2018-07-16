790935f12decacc9687c63e2f5b8407cf302e387
Omer Ozarslan <omerfaruko@gmail.com>
2018-07-16 09:08:33 -0500

    Add fixtures and rename test suite to Core

--- tests/check_stream_api.c
+++ tests/check_stream_api.c
@@ -1,49 +1,114 @@
-#include<check.h>
+#include<stdio.h>
 #include<stdlib.h>
 
+#include<check.h>
+
+#include "utils.h"
 #include "zidx.h"
+
+#ifndef RANDOM_SEED
+#define RANDOM_SEED (0UL)
+#endif
+
+#ifndef COMP_FILE_LENGTH
+#define COMP_FILE_LENGTH (100 * (1 << 20))
+#endif
+
+const char *comp_file_path;
+zidx_comp_stream *comp_stream;
+
+void unchecked_setup()
+{
+    comp_file_path = get_random_compressed_file(RANDOM_SEED, COMP_FILE_LENGTH);
+
+    ck_assert_msg(comp_file_path, "Couldn't create temporary compressed file.");
+}
+
+void unchecked_teardown()
+{
+    int comp_file_deleted;
+    comp_file_deleted = remove(comp_file_path);
+
+    ck_assert_msg(comp_file_deleted == 0, "Couldn't remove temporary compressed "
+                                          "file.");
+}
+
+void setup()
+{
+    int zidx_ret;
+    FILE *f;
+    const char *msg;
+
+    f = fopen(comp_file_path, "rb");
+    if (f == NULL) {
+        msg = "Couldn't open compressed file.";
+        goto fail;
+    }
+
+    comp_stream = malloc(sizeof(zidx_comp_stream));
+    if (comp_stream == NULL) {
+        msg = "Couldn't allocate space form zidx compressed stream.";
+        goto fail;
+    }
+
+    zidx_ret = zidx_comp_file_init(comp_stream, f);
+    if (zidx_ret != 0) {
+        msg = "Couldn't initialize zidx file stream.";
+        goto fail;
+    }
+
+    return;
+
+fail:
+    if (f) fclose(f);
+    free(comp_stream);
+    ck_abort_msg(msg);
+}
+
+void teardown()
+{
+    FILE *f;
+
+    f = comp_stream->context;
+
+    free(comp_stream);
+
+    ck_assert_msg(fclose(f) == 0, "File couldn't be closed.");
+}
 
 START_TEST(test_comp_file_init)
 {
-    void *context;
-    zidx_comp_stream *comp_stream;
-
-    context     = (void *) 0xBEEF;
-    comp_stream = malloc(sizeof(zidx_comp_stream));
-
-    ck_assert(zidx_comp_file_init(comp_stream, context) == 0);
-
     ck_assert(comp_stream->read    == zidx_raw_file_read);
     ck_assert(comp_stream->seek    == zidx_raw_file_seek);
     ck_assert(comp_stream->tell    == zidx_raw_file_tell);
     ck_assert(comp_stream->length  == zidx_raw_file_length);
     ck_assert(comp_stream->eof     == zidx_raw_file_eof);
     ck_assert(comp_stream->error   == zidx_raw_file_error);
-    ck_assert(comp_stream->context == context);
-
-    free(comp_stream);
+    ck_assert(comp_stream->context != NULL);
 }
 END_TEST
 
 START_TEST(test_comp_file_read)
 {
-    // TODO
 }
 END_TEST
 
-Suite* stream_api_suite()
+Suite* libzidx_test_suite()
 {
     Suite *s;
-    TCase *tc_stream_api;
+    TCase *tc_core;
 
     s = suite_create("libzidx");
 
-    tc_stream_api = tcase_create("Stream API");
+    tc_core = tcase_create("Core");
 
-    tcase_add_test(tc_stream_api, test_comp_file_init);
-    tcase_add_test(tc_stream_api, test_comp_file_read);
+    tcase_add_unchecked_fixture(tc_core, unchecked_setup, unchecked_teardown);
+    tcase_add_checked_fixture(tc_core, setup, teardown);
 
-    suite_add_tcase(s, tc_stream_api);
+    tcase_add_test(tc_core, test_comp_file_init);
+    tcase_add_test(tc_core, test_comp_file_read);
+
+    suite_add_tcase(s, tc_core);
 
     return s;
 }
@@ -54,7 +119,7 @@ int main()
     Suite *s;
     SRunner *sr;
 
-    s = stream_api_suite();
+    s = libzidx_test_suite();
     sr = srunner_create(s);
 
     srunner_run_all(sr, CK_VERBOSE);
