037d4e8c94b359436fd41edc279948ce99054fe2
Omer Ozarslan <omerfaruko@gmail.com>
2018-07-17 15:10:16 -0500

    Get verbosity of tests from environment var
    Use CK_VERBOSITY to indicate verbosity. Default verbosity is CK_NORMAL as of
    libcheck 0.12.0

--- tests/check_libzidx.c
+++ tests/check_libzidx.c
@@ -251,7 +251,7 @@ int main()
     s = libzidx_test_suite();
     sr = srunner_create(s);
 
-    srunner_run_all(sr, CK_NORMAL);
+    srunner_run_all(sr, CK_ENV);
 
     number_failed = srunner_ntests_failed(sr);
     srunner_free(sr);
